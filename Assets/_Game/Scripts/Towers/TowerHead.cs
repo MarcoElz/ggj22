aef15f6936355bf6413ef0ed6b015163d3e8c82e
MarcoElz <marco.eelizondo@gmail.com>
2022-01-30 11:49:16 -0600

    Add all new towers and art assets into the main scene

---
+++ Assets/_Game/Scripts/Towers/TowerHead.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Game.Towers
+{
+    public class TowerHead : MonoBehaviour
+    {
+        [SerializeField] private Transform bulletOrigin = default;
+
+        public Transform BulletOrigin => bulletOrigin;
+    }
+}
--- Assets/_Game/Scripts/Towers/TowerInstances/ShooterTower.cs
+++ Assets/_Game/Scripts/Towers/TowerInstances/ShooterTower.cs
@@ -8,14 +8,24 @@ namespace _Game.Towers
 {
     public class ShooterTower : AbstractTower
     {
-        [SerializeField] private Transform head = default;
-        [SerializeField] private Transform bulletOrigin = default;
-        
         private ShooterData currentData;
         private ShooterData CurrentData => currentData != null ? currentData : currentData = (ShooterData) CurrentAbstractData;
 
         private float timeBetweenAttacks;
         private float timeOfLastAttack;
+
+        private TowerHead head;
+
+        private TowerHead Head
+        {
+            get
+            {
+                if (head == null)
+                    head = GetComponentInChildren<TowerHead>();
+
+                return head;
+            }
+        }
         
         protected override void Awake()
         {
@@ -40,6 +50,7 @@ namespace _Game.Towers
         {
             base.OnUpgraded();
             RefreshStats();
+            head = GetComponentInChildren<TowerHead>();
         }
 
         public void Shoot()
@@ -48,9 +59,13 @@ namespace _Game.Towers
             if(target == null)
                 return;
 
-            head.LookAt(target.transform);
+            Head.transform.LookAt(target.transform);
+            var euler = Head.transform.eulerAngles;
+            euler.x = 0f;
+            euler.z = 0f;
+            Head.transform.eulerAngles = euler;
             
-            var bullet = PoolManager.Spawn(CurrentData.BulletPrefab, bulletOrigin.position, bulletOrigin.rotation);
+            var bullet = PoolManager.Spawn(CurrentData.BulletPrefab, Head.BulletOrigin.position, head.BulletOrigin.rotation);
             bullet.Init(target, CurrentData.Damage);
         }
 
